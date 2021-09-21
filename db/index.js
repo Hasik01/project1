8e044cf1a93fc113d131a8db0aada48ecd647429
Hasik01 <85454084+Hasik01@users.noreply.github.com>
2021-09-21 18:59:20 +0400

    Update index.js

--- db/index.js
+++ db/index.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://127.0.0.1:27017/basic', {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect('mongodb://127.0.0.1:27017', {useNewUrlParser: true, useUnifiedTopology: true});
 
 var db = mongoose.connection;
 
